faed21e94f662f646dee869345deadb426b15aa3
awstine <awstinejessy@gmail.com>
2026-02-03 06:42:31 +0000

    updated the bottom bar

--- app/src/main/java/com/example/myapplication/screens/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/MainScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -141,6 +142,7 @@ fun StyledBottomBar(navController: NavHostController, items: List<BottomNavItem>
         Row(
             modifier = Modifier
                 .fillMaxWidth()
+                .navigationBarsPadding()
                 .padding(vertical = 40.dp),
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically
